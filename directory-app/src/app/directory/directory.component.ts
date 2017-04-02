5a127026e418fd232e1e96d7543b90aa45857439
Emelie Johansson <emelietejohansson@gmail.com>
2017-04-02 17:46:45 +0200

    Testing route params

--- directory-app/src/app/directory/directory.component.ts
+++ directory-app/src/app/directory/directory.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-directory',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectoryComponent implements OnInit {
 
-  constructor() { }
+  ninjaCoder: string;
+
+  constructor(private route: ActivatedRoute) {
+    this.ninjaCoder = route.snapshot.params['ninjaCoder'];
+  }
 
   ngOnInit() {
   }
