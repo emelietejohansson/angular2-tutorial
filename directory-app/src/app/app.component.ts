e60069771b828992a3d2397198ec696a22e69574
Emelie Johansson <emelietejohansson@gmail.com>
2017-04-02 16:09:08 +0200

    Adding routes

--- directory-app/src/app/app.component.ts
+++ directory-app/src/app/app.component.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   //template: '<h1>{{name}}</h1>',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app works!';
-  name = 'Emelie';
-  ninjaCoder = {
-      name: "Emelie",
-      belt: "Blue"
-  };
+    title = 'app works!';
+    name = 'Emelie';
+    ninjaCoder = {
+        name: "Emelie",
+        belt: "Blue"
+    };
 
-yell(e) {
-    alert("I am yelling!");
-    console.log(e);
-}
-
+    yell(e) {
+        alert("I am yelling!");
+        console.log(e);
+    }
 }
